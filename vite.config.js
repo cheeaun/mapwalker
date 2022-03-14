203107cc305dc7586545072e95e3d44c16fa6779
Lim Chee Aun <cheeaun@gmail.com>
2022-03-14 22:44:44 +0800

    Need base ./

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [preact()],
   server: {
     https: true, // because Geolocation
