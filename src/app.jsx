ff0aa5d26e803901f083f9bcd76f4f6975048fd6
Lim Chee Aun <cheeaun@gmail.com>
2022-03-26 23:21:47 +0800

    Add auto/light/dark mode selector, in new Options sheet

--- src/app.jsx
+++ src/app.jsx
@@ -182,6 +182,7 @@ export function App() {
 
   const [mapTextLayerID, setMapTextLayerID] = useState(null);
   const [legendSheetOpen, setLegendSheetOpen] = useState(false);
+  const [optionsSheetOpen, setOptionsSheetOpen] = useState(false);
   const [markerSheetOpen, setMarkerSheetOpen] = useState(false);
   const [aboutSheetOpen, setAboutSheetOpen] = useState(
     LS.get('not-first-time') ? false : true,
@@ -364,6 +365,27 @@ export function App() {
       padding: 100,
     });
   }, []);
+
+  const [theme, setTheme] = useState(LS.get('theme') || 'auto');
+  useEffect(() => {
+    const html = document.documentElement;
+    const colorSchemeMeta = document.querySelector('meta[name="color-scheme"]');
+    if (theme === 'light') {
+      html.classList.add('is-light');
+      html.classList.remove('is-dark');
+      colorSchemeMeta?.setAttribute('content', 'light');
+      LS.set('theme', theme);
+    } else if (theme === 'dark') {
+      html.classList.add('is-dark');
+      html.classList.remove('is-light');
+      colorSchemeMeta?.setAttribute('content', 'dark');
+      LS.set('theme', theme);
+    } else {
+      html.classList.remove('is-dark', 'is-light');
+      colorSchemeMeta?.setAttribute('content', 'light dark');
+      LS.del('theme');
+    }
+  }, [theme]);
 
   return (
     <div class={`${overviewMapExpanded ? 'split-view' : ''}`}>
@@ -587,6 +609,20 @@ export function App() {
                 <path
                   fill="currentColor"
                   d="m11 18h2v-2h-2zm1-16c-5.52 0-10 4.48-10 10s4.48 10 10 10 10-4.48 10-10-4.48-10-10-10zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"
+                />
+              </svg>
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setOptionsSheetOpen(true);
+              }}
+              title="Options"
+            >
+              <svg height="24" viewBox="0 0 24 24" width="24">
+                <path
+                  fill="currentColor"
+                  d="m19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65c-.03-.24-.24-.42-.49-.42h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64zm-1.98-1.71c.04.31.05.52.05.73s-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7zm-5.45-3.27c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"
                 />
               </svg>
             </button>
@@ -816,6 +852,32 @@ export function App() {
                 )}
               </dd>
             </dl>
+          </div>
+        </BottomSheet>
+        <BottomSheet
+          open={optionsSheetOpen}
+          onDismiss={() => {
+            setOptionsSheetOpen(false);
+          }}
+        >
+          <div class="bottom-sheet-container options-sheet-container">
+            <h2>Options</h2>
+            <p>
+              <label>
+                Theme:{' '}
+                <select
+                  id="theme-switcher"
+                  value={theme}
+                  onChange={(e) => {
+                    setTheme(e.target.value);
+                  }}
+                >
+                  <option value="auto">Auto</option>
+                  <option value="light">Light</option>
+                  <option value="dark">Dark</option>
+                </select>
+              </label>
+            </p>
           </div>
         </BottomSheet>
         <BottomSheet
