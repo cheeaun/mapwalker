02f353dbdf6f8d7a40dc69f6ce28392f40fd6588
Lim Chee Aun <cheeaun@gmail.com>
2022-03-31 01:25:57 +0800

    Start using unplugin-icons

--- src/app.jsx
+++ src/app.jsx
@@ -32,6 +32,18 @@ import walkDotPurpleImgURL from '../assets/walk-dot-purple.png';
 import walkDotRedImgURL from '../assets/walk-dot-red.png';
 import iconImgURL from '../assets/icon-192.png';
 
+import IconWalk from '~icons/ic/round-directions-walk';
+import IconAbout from '~icons/mdi/information-outline';
+import IconLegend from '~icons/mdi/help-circle-outline';
+import IconOptions from '~icons/mdi/cog-outline';
+import IconActions from '~icons/mdi/view-grid-outline';
+import IconExpand from '~icons/mdi/fullscreen';
+import IconCollapse from '~icons/mdi/fullscreen-exit';
+import IconRemove from '~icons/mdi/close-box-outline';
+import IconFit from '~icons/mdi/fit-to-screen-outline';
+import IconFly from '~icons/mdi/ufo-outline';
+import IconRoutes from '~icons/mdi/routes';
+
 const { VITE_MAPBOX_ACCESS_TOKEN: MAPBOX_ACCESS_TOKEN, DEV } = import.meta.env;
 
 const OVERVIEW_ZOOM_BACK = 2;
@@ -631,12 +643,7 @@ export function App() {
               }}
               title="About"
             >
-              <svg height="24" viewBox="0 0 24 24" width="24">
-                <path
-                  fill="currentColor"
-                  d="m11 7h2v2h-2zm0 4h2v6h-2zm1-9c-5.52 0-10 4.48-10 10s4.48 10 10 10 10-4.48 10-10-4.48-10-10-10zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-                />
-              </svg>
+              <IconAbout />
             </button>
             <button
               type="button"
@@ -645,12 +652,7 @@ export function App() {
               }}
               title="Legend"
             >
-              <svg height="24" viewBox="0 0 24 24" width="24">
-                <path
-                  fill="currentColor"
-                  d="m11 18h2v-2h-2zm1-16c-5.52 0-10 4.48-10 10s4.48 10 10 10 10-4.48 10-10-4.48-10-10-10zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"
-                />
-              </svg>
+              <IconLegend />
             </button>
             <button
               type="button"
@@ -659,12 +661,7 @@ export function App() {
               }}
               title="Options"
             >
-              <svg height="24" viewBox="0 0 24 24" width="24">
-                <path
-                  fill="currentColor"
-                  d="m19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65c-.03-.24-.24-.42-.49-.42h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64zm-1.98-1.71c.04.31.05.52.05.73s-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7zm-5.45-3.27c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"
-                />
-              </svg>
+              <IconOptions />
             </button>
             <button
               type="button"
@@ -675,16 +672,7 @@ export function App() {
               title="Actions"
               disabled={loading}
             >
-              {loading ? (
-                <div class="loading" />
-              ) : (
-                <svg height="24" viewBox="0 0 24 24" width="24">
-                  <path
-                    fill="currentColor"
-                    d="m3 3v8h8v-8zm6 6h-4v-4h4zm-6 4v8h8v-8zm6 6h-4v-4h4zm4-16v8h8v-8zm6 6h-4v-4h4zm-6 4v8h8v-8zm6 6h-4v-4h4z"
-                  />
-                </svg>
-              )}
+              {loading ? <div class="loading" /> : <IconActions />}
             </button>
           </div>
         </Map>
@@ -716,11 +704,13 @@ export function App() {
             ).style.display = hideNavControl ? 'none' : 'block';
 
             // check  if geolocationGeoJSON is in view
-            const overviewMapBounds = overviewMapRef.current.getBounds();
-            const isInBounds = overviewMapBounds.contains(
-              geolocationGeoJSON?.features[0].geometry.coordinates,
-            );
-            setShowMiniLocationButton(!isInBounds);
+            if (geolocationGeoJSON) {
+              const overviewMapBounds = overviewMapRef.current.getBounds();
+              const isInBounds = overviewMapBounds.contains(
+                geolocationGeoJSON?.features[0].geometry.coordinates,
+              );
+              setShowMiniLocationButton(!isInBounds);
+            }
           }}
           onMove={renderOverviewMapArrow}
         >
@@ -764,21 +754,7 @@ export function App() {
               setOverviewMapExpanded(!overviewMapExpanded);
             }}
           >
-            {overviewMapExpanded ? (
-              <svg height="24" viewBox="0 0 24 24" width="24">
-                <path
-                  fill="currentColor"
-                  d="m5 16h3v3h2v-5h-5zm3-8h-3v2h5v-5h-2zm6 11h2v-3h3v-2h-5zm2-11v-3h-2v5h5v-2z"
-                />
-              </svg>
-            ) : (
-              <svg height="24" viewBox="0 0 24 24" width="24">
-                <path
-                  fill="currentColor"
-                  d="m7 14h-2v5h5v-2h-3zm-2-4h2v-3h3v-2h-5zm12 7h-3v2h5v-5h-2zm-3-12v2h3v3h2v-5z"
-                />
-              </svg>
-            )}
+            {overviewMapExpanded ? <IconCollapse /> : <IconExpand />}
           </button>
           {overviewMapExpanded && showMiniLocationButton && (
             <button
@@ -855,7 +831,10 @@ export function App() {
                 LS.set('not-first-time', true);
               }}
             >
-              <span>🚶</span> Start walking now!
+              <span>
+                <IconWalk />
+              </span>{' '}
+              Start walking now!
             </button>
           </div>
         </BottomSheet>
@@ -990,7 +969,10 @@ export function App() {
                     setMarkerPinned(false);
                   }}
                 >
-                  <span>❌</span> Remove marker
+                  <span>
+                    <IconRemove />
+                  </span>{' '}
+                  Remove marker
                 </button>
                 <button
                   type="button"
@@ -1010,7 +992,10 @@ export function App() {
                       setMarkerSheetOpen(false);
                     }}
                   >
-                    <span>🔭</span> Zoom whole route
+                    <span>
+                      <IconFit />
+                    </span>{' '}
+                    Zoom whole route
                   </button>
                 )}
                 <button
@@ -1022,7 +1007,10 @@ export function App() {
                     setMarkerSheetOpen(false);
                   }}
                 >
-                  <span>🔍</span> Fly to marker
+                  <span>
+                    <IconFly />
+                  </span>{' '}
+                  Fly to marker
                 </button>
                 <button
                   type="button"
@@ -1049,7 +1037,10 @@ export function App() {
                     zoomWholeRoute(results);
                   }}
                 >
-                  <span>🔃</span> Generate walk routes to marker
+                  <span>
+                    <IconRoutes />
+                  </span>{' '}
+                  Generate walk routes to marker
                 </button>
               </>
             ) : (
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import Icons from 'unplugin-icons/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    Icons({
+      scale: 1.75,
+      compiler: 'jsx',
+      jsx: 'preact',
+    }),
+  ],
   server: {
     https: true, // because Geolocation
     host: true,
